2c7d225b66196b5761212678fe994cecd728f4f7
SetuSouls03 <setusouls@gmail.com>
2026-08-13 17:33:51 +0530

    BIOMETRIC -3

--- biomed-backend/app/config.py
+++ biomed-backend/app/config.py
@@ -12,9 +12,9 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     
     # WebAuthn
-    WEBAUTHN_RP_ID: str = "localhost"
+    WEBAUTHN_RP_ID: str = "biomed-auth.netlify.app"
     WEBAUTHN_RP_NAME: str = "BioMed Pharmacy"
-    WEBAUTHN_ORIGIN: str = "http://localhost:3000"
+    WEBAUTHN_ORIGIN: str = "https://biomed-auth.netlify.app/"
     
     # CORS
     ALLOWED_ORIGINS: List[str] = ["http://localhost:3000", "http://localhost:8000", "https://biomed-2nq9.onrender.com/"]
